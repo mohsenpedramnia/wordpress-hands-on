7084eabbde5974a9670c4540542e9fd0c2f805bc
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2020-07-11 11:48:41 +0300

    WordPress initial commit

---
+++ themes/college-for-all-theme/functions.php
@@ -0,0 +1,21 @@
+<?php 
+
+function college_files() {
+  wp_enqueue_script('main-college-js',get_theme_file_uri('/js/scripts-bundled.js'),NULL, microtime(),true);
+  wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('college_main_style', get_stylesheet_uri(),NULL, microtime());
+}
+
+
+add_action('wp_enqueue_scripts', 'college_files');
+
+function college_features() {
+
+  add_theme_support('title-tag');
+}
+
+
+add_action('after_setup_theme', 'college_features');
+
+?>
